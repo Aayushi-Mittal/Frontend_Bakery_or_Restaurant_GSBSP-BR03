777e0235d187db07f9e40dd17b0b5695491f9eaa
Aayushi-Mittal <aayushimittal088@gmail.com>
2020-07-19 23:43:04 +0530

    made navbar and carousel responsive

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 alert("Welcome to Dawat-e-Hindustan");
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
